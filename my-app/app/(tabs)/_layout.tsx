1a842bdac3b61de74dd2c9ee2894fd944b043aec
Monisha Krothapalli <mkrothapalli123@gmail.com>
2024-07-21 22:48:11 -0700

    updated tab icon

--- my-app/app/(tabs)/_layout.tsx
+++ my-app/app/(tabs)/_layout.tsx
@@ -17,9 +17,9 @@ export default function TabLayout() {
       <Tabs.Screen
         name="index"
         options={{
-          title: 'Home',
+          title: 'Task Manager',
           tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon name={focused ? 'home' : 'home-outline'} color={color} />
+            <TabBarIcon name={focused ? 'checkmark-done' : 'checkmark-outline'} color={color} />
           ),
         }}
       />
